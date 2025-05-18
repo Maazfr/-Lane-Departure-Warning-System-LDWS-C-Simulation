38fdde8eb426c02a5b1b9c76528992b5c9b757b3
Maazfr <146356081+Maazfr@users.noreply.github.com>
2025-05-18 20:38:37 +0530

    Update sketch.ino

--- wokwi project/sketch.ino
+++ wokwi project/sketch.ino
@@ -1,3 +1,10 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 4);
+
+
+
+
 class LED
 {
 int pin;
@@ -33,6 +40,7 @@ class BUZZER
 LED ledLeft(13);
 LED ledMidd(12);
 LED ledRite(11);
+LED ledOver(8);
 BUZZER buzzLeft(10);
 BUZZER buzzRite(9);
 
@@ -68,37 +76,78 @@ public:
 
   void checkLaneDeparture() {
     int speed = readSpeed();
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Speed :");
+    lcd.print(speed);
+    lcd.print("km\h");
     
+    lcd.setCursor(0,1);
+
     if (!sensor.leftDetected && sensor.rightDetected) {
       Serial.println(" Drifting LEFT! Correct your steering.");
+      lcd.print("Drifting Left");
       ledLeft.on();
       ledMidd.off();
       ledRite.off();
-      if(speed > 75) buzzLeft.beep(); 
+      ledOver.off();
+      if(speed > 75) 
+      {
+        lcd.setCursor(0, 3);
+        buzzLeft.beep();
+        Serial.println("Over Speeding! Reduce Speed NOW");
+        lcd.print("OVER SPEEDING");
+        ledOver.on();
+      }
     }
     else if (sensor.leftDetected && !sensor.rightDetected) {
       Serial.println(" Drifting RIGHT! Correct your steering.");
+      lcd.print("Drifting Right");
       ledLeft.off();
       ledMidd.off();
       ledRite.on();
-      if(speed > 75) buzzRite.beep();
+      ledOver.off();
+      if(speed > 75) 
+      {
+        lcd.setCursor(0, 4);
+        buzzRite.beep();
+        Serial.println("Over Speeding! Reduce Speed NOW");
+        lcd.print("OVER SPEEDING");
+        ledOver.on();
+      }
     }
     else if (!sensor.leftDetected && !sensor.rightDetected) {
       Serial.println(" Completely off-lane! Emergency!");
+      lcd.print("~~EMERGENCY~~");
       ledLeft.on();
       ledMidd.off();
       ledRite.on();
+      ledOver.off();
       if(speed > 75)
       {
+        lcd.setCursor(0, 3);
       buzzLeft.beep();
       buzzRite.beep();
+      Serial.println("Over Speeding! Reduce Speed NOW");
+      lcd.print("OVER SPEEDING");
+      ledOver.on();
       }
     }
     else {
       Serial.println(" In lane.");
+      lcd.print("IN LANE");
       ledLeft.off();
       ledMidd.on();
       ledRite.off();
+      ledOver.off();
+      if(speed > 75)
+      {
+        lcd.setCursor(0, 3);
+        Serial.println("Over Speeding! Reduce Speed NOW");
+        lcd.print("OVER SPEEDING");
+        ledOver.on();
+      }
     }
   }
 };
@@ -108,6 +157,14 @@ LaneDepartureSystem laneSystem(laneSensor);
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0)); 
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("LDWS starting");
+  delay(1000);
+  lcd.clear();
+  
 }
 
 void loop() {
